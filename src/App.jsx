652ce633acbd25349fd1d255e8551ba83fcbb587
josemiguelcarvajal07 <62575167+josemiguelcarvajal07@users.noreply.github.com>
2023-06-20 16:31:59 -0500

    init vite project

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import useWordGame from "./hooks/useWordGame"
+
+export default function App() {
+    const {
+        textBoxRef, 
+        handleChange, 
+        text, 
+        isTimeRunning, 
+        timeRemaining, 
+        startGame, 
+        wordCount
+    } = useWordGame(10)
+    
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea
+                ref={textBoxRef}
+                onChange={handleChange}
+                value={text}
+                disabled={!isTimeRunning}
+            />
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button 
+                onClick={startGame}
+                disabled={isTimeRunning}
+            >
+                Start
+            </button>
+            <h3>Word count: {wordCount}</h3>
+        </div>
+    )
+}
